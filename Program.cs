8aebb1a4b0901bab89ec83f1727ebcad4b6330c6
Christopher Prado <cprad18@gmail.com>
2022-10-21 10:11:46 -0700

    First Push

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//Christopher Prado
+//Date Revised: 12OCT22
+//Mini Challenge #3 "Build an app where you ask 2 questions 
+//We will ask the users name
+//We will ask what time the user woke up
+// We will print out user name and what time they woke up
+//Make sure you print the results in one line
+//Peer Review by: 
+
+Console.Clear();
+
+Console.WriteLine("What is your name?");
+
+string myName = Console.ReadLine();
+
+Console.WriteLine("What time did you wake up?");
+
+string myTime = Console.ReadLine();
+
+Console.WriteLine(myName + " woke up today at " + myTime);
+
+
+
+
